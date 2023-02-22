a6636fdbda813026f590e089b20a17ba8146344b
adedwi1808 <adedwip1808@gmail.com>
2023-02-22 11:41:38 +0700

    Initial Commit

---
+++ TechnoPartner-Test/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TechnoPartner-Test
+//
+//  Created by Ade Dwi Prayitno on 22/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TechnoPartner-Test/TechnoPartner_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechnoPartner_TestApp.swift
+//  TechnoPartner-Test
+//
+//  Created by Ade Dwi Prayitno on 22/02/23.
+//
+
+import SwiftUI
+
+@main
+struct TechnoPartner_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
