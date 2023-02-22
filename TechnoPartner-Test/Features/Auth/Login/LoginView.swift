4cb5388f8033af2e1185f1e132c0727083bfda38
adedwi1808 <adedwip1808@gmail.com>
2023-02-22 11:54:12 +0700

    ADD: assets required from test

---
+++ TechnoPartner-Test/Features/Auth/Login/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  TechnoPartner-Test
+//
+//  Created by Ade Dwi Prayitno on 22/02/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
